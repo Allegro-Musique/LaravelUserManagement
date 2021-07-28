b9346e198416f65b253e6593ca23a015e0c99201
MAREGA Oumar <oumarmarega18@gmail.com>
2021-07-28 13:05:34 +0200

    composer request

--- src/Http/Requests/Admin/StoreUser.php
+++ src/Http/Requests/Admin/StoreUser.php
@@ -24,19 +24,16 @@ class StoreUser extends FormRequest
     public function rules()
     {
         $userTable       = config("laravel_user_management.users_table");
-        $departmentTable = config("laravel_user_management.user_department_table");
         $tableNames      = config('permission.table_names');
 
         return [
             'first_name'    => 'required|string',
             'last_name'     => 'required|string',
             'email'         => "nullable|email|unique:$userTable,email",
-            'mobile'        => "required|unique:$userTable,mobile",
+            'phone'        => "required|unique:$userTable,mobile",
             'password'      => 'required|min:6',
             'roles'         => 'nullable|array',
             'roles.*'       => 'nullable|exists:'. $tableNames['roles']. ',name',
-            'departments'   => 'nullable|array',
-            'departments.*' => "nullable|exists:$departmentTable,id",
         ];
     }
 }
